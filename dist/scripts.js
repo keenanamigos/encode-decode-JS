1490e1fed0481a06b77351d3b82b63552494b5bd
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-08 23:03:55 -0500

    Run gulp watch to transpile JS

---
+++ dist/scripts.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function getIntermediateDecimalValue(integer) {
+    if (integer >= -8192 || integer <= 8191) {
+        var fourteenBitUpperBound = 8192;
+
+        var intermediateDecimalValue = integer + fourteenBitUpperBound;
+
+        if (intermediateDecimalValue >= 0 && intermediateDecimalValue <= 16383) {
+            return intermediateDecimalValue;
+        }
+    }
+}
+
+function getIntermediateHexValue(integer) {
+    return integer.toString(16);
+}
+
+function getEncodedHexValue(hexString) {
+    console.log("NOT IMPLEMENTED");
+}
+
+// convertIntToTextEncoding [retuns a 4 character string]
+// 8912 is added to EVERY integer passed in
+// parseInt && !isNaN to check
+
+// Decode
+function convertIntermediateDecimalToUnencodedDecimal(integer) {
+    if (integer >= 0 && integer <= 16383) {
+        var fourteenBitUpperBound = 8192;
+
+        return integer - fourteenBitUpperBound;
+    }
+}
+
+function convertIntermediateHexToIntermediateDecimal(string) {
+    return parseInt(string, 16);
+}
+
+function convertEncodedHexToIntermediateHex(string) {
+    console.log("NOT IMPLEMENTED");
+}
+
+module.exports = {
+    getIntermediateDecimalValue: getIntermediateDecimalValue,
+    getIntermediateHexValue: getIntermediateHexValue,
+    getEncodedHexValue: getEncodedHexValue,
+    convertIntermediateDecimalToUnencodedDecimal: convertIntermediateDecimalToUnencodedDecimal,
+    convertIntermediateHexToIntermediateDecimal: convertIntermediateHexToIntermediateDecimal,
+    convertEncodedHexToIntermediateHex: convertEncodedHexToIntermediateHex
+};
