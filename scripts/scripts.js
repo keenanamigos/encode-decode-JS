fc0eae57d8a88ccb730da837b5b8005cb99acb01
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-08 22:34:20 -0500

    Remove comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,3 @@
-/* TODO: Two functions Total (to solve, more if I need) */
-
-// 14-bit range is from -8192 to +8191
-
-// getIntermediateDecimal (int)
 function getIntermediateDecimalValue(integer) {
     if (integer >= -8192 || integer <= 8191) {
         const fourteenBitUpperBound = 8192;
@@ -16,26 +11,19 @@ function getIntermediateDecimalValue(integer) {
 }
 module.exports.getIntermediateDecimalValue = getIntermediateDecimalValue;
 
-// getIntermediateHexValue (int) #Hexideicmal is base 16
 function getIntermediateHexValue(integer) {
     return integer.toString(16);
 }
 module.exports.getIntermediateHexValue = getIntermediateHexValue;
 
-// console.log(getIntermediateHexValue(16383));
-
-// getEncodedHexValue (string)
 function getEncodedHexValue(hexString) {
-    
+
 }
 module.exports.getEncodedHexValue = getEncodedHexValue;
-
-console.log(getEncodedHexValue("3fff"));
 
 // convertIntToTextEncoding [retuns a 4 character string]
     // 8912 is added to EVERY integer passed in
     // parseInt && !isNaN to check
-// convertEncodedTextToInt
 
 // Decode
 function convertIntermediateDecimalToUnencodedDecimal(integer) {
